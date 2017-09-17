a482dde1d13a437f1ccdf0416e174d56492105f2
Gina Antonini <ginamantonini@gmail.com>
2017-09-17 10:01:12 -0500

    made the gifs appear on the page

--- javascripts/data.js
+++ javascripts/data.js
@@ -42,4 +42,8 @@ const initializer = () => {
 	loadGifs(whenGifsLoad, errorFunction);
 };
 
-module.exports = initializer;
+const getGifs = () => {
+	return gifArray;
+};
+
+module.exports = {initializer, getGifs};
